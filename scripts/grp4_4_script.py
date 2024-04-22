8bcfad401136d1eb6b3f4a9cfef24e3ea082c219
madanbaduwal <madanbaduwal100@gmail.com>
2024-04-22 17:14:07 -0500

    .

--- scripts/grp4_4_script.py
+++ scripts/grp4_4_script.py
@@ -1,41 +1,37 @@
-import itertools
-
-def generate_substituted_words(word):
-    substituted_words = []
-    for indices in itertools.combinations(range(len(word)), 2):
-        for replacement in itertools.product('0123456789', repeat=2):
-            substituted_word = list(word)
-            for index, digit in zip(indices, replacement):
-                substituted_word[index] = digit
-            substituted_words.append(''.join(substituted_word))
-    return substituted_words
-
-def generate_wordlist(input_file):
-    characters = 'abcdefghijklmnopqrstuvwxyz'
-    digits = '0123456789'
-    specials = '!@#$%&*-=+'
-    padding = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%&*-=+'
-
-    wordlist = []
-    with open(input_file, 'r') as f:
-        for line in f:
-            word = line.strip()
-            if 8 <= len(word) <= 10:
-                for word_chars in itertools.combinations(characters, len(word) - 2):
-                    for caps_padding in itertools.combinations(padding, 2):
-                        for special in specials:
-                            word = ''.join(word_chars)
-                            word = word.capitalize() + ''.join(caps_padding) + special
-                            wordlist.extend(generate_substituted_words(word))
-    return wordlist
-
 def create_subset_wordlist(input_file, output_file):
-    wordlist = generate_wordlist(input_file)
+    with open(input_file, 'r') as f:
+        wordlist = f.read().splitlines()
+
+    subset_wordlist = []
+
+    for word in wordlist:
+        # Rule 1: 6-letter word, all lowercase
+        if len(word) == 6:
+            subset_wordlist.append(word.lower())
+        
+        # Rule 2: 7-letter word, first letter capitalized
+        elif len(word) == 7:
+            subset_wordlist.append(word.capitalize())
+        
+        # Rule 3: 8-10 letters, two substitutions, first letter caps, one additional caps padding
+        elif 8 <= len(word) <= 10:
+            for i in range(len(word)):
+                for j in range(i+1, len(word)):
+                    for digit1 in '0123456789':
+                        for digit2 in '0123456789':
+                            new_word = list(word)
+                            new_word[i] = digit1
+                            new_word[j] = digit2
+                            # First letter capitalized
+                            new_word[0] = new_word[0].upper()
+                            subset_wordlist.append(''.join(new_word))
 
     with open(output_file, 'w') as f:
-        f.write('\n'.join(wordlist))
+        f.write('\n'.join(subset_wordlist))
 
 # Usage example
-input_file = 'wordlist.txt'  # Replace with your input wordlist file
+input_file = 'wordlist.txt'  # Replace with the path to your input wordlist file
 output_file = 'grp4_4_wordlist.txt'  # Replace with the desired output file name
 create_subset_wordlist(input_file, output_file)
+
+
